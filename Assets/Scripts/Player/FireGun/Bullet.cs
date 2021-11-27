e4ae7f3566f7a9fa144b8618ea8bb4cfface46d8
Canberk Bekiroğlu <canberk.hesap@gmail.com>
2021-11-27 17:16:35 +0300

    FireGun and Bullet
    Added fire and bullet script

---
+++ Assets/Scripts/Player/FireGun/Bullet.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] float bulletSpeed;
+    
+
+    private void Update() {
+        bulletMovement();
+    }
+
+    void bulletMovement()
+    {
+        transform.position = transform.position + transform.forward * bulletSpeed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.tag == "Obstacle")
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+}
---
+++ Assets/Scripts/Player/FireGun/FireGun.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireGun : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private RaycastHit hit;
+    [HideInInspector]public GameObject hitObject;
+    [HideInInspector]public Vector3 hitPoint;
+    [SerializeField] float rayDistance = 100f;
+    [SerializeField] float fireRate = 0.4f;
+    [SerializeField] Camera camera;
+    [SerializeField] Transform gunPoint;
+    [SerializeField] GameObject bulletPrefab;
+
+
+
+    private float nextFire = 0.0f;
+    private bool fireCheck = true;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Physics.Raycast(camera.transform.position, camera.transform.forward, out hit, rayDistance))
+        {
+            hitObject = hit.transform.gameObject;
+            hitPoint = hit.point;
+        }
+        else
+        {
+            hitObject = null;
+
+        }
+
+        //Functions
+        fireGun();
+    }
+
+
+    void fireGun()
+    {
+        if(Input.GetMouseButton(1) && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            createBullet();
+        }
+    }
+
+    void createBullet()
+    {
+        GameObject bullet;
+        bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
+    }
+    
+}
